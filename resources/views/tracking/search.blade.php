f8bafc2b0adef0dbb4403b3549c31e9ae7d93155
softmitend <softmitend@gmail.com>
2026-09-12 23:11:34 +0700

    Hapus teks Lacak Pesanan dari topbar tracking

--- resources/views/tracking/search.blade.php
+++ resources/views/tracking/search.blade.php
@@ -3,7 +3,6 @@
         <div class="tracking-search-shell">
             <div class="detail-topbar">
                 <a class="top-back" href="{{ route('services.index') }}" aria-label="Kembali ke layanan"><x-public-icon name="arrow-left" :size="18" /></a>
-                <strong>Lacak Pesanan</strong>
             </div>
 
             @include('tracking.partials.search-panel')
